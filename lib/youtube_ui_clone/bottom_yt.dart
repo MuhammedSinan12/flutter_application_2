e9affa756b044dca3168ac8f40601f9b86b773a3
MuhammedSinan12 <sinusinu1222@.com>
2022-12-12 14:13:18 +0530

    yt

---
+++ lib/youtube_ui_clone/bottom_yt.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BottomBarYt extends StatelessWidget {
+  const BottomBarYt({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/youtube_ui_clone/datas.dart
@@ -0,0 +1,35 @@
+List<Map> VideoData = [
+  {
+    'thumb':
+        'https://i.ytimg.com/vi/h5NvTTOlOtI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBeklIbvLYYueQ-CC9fDQLTrg9etw',
+    'duration': '2:44',
+    'title': 'Hydraulic Press vs Lamborgini',
+    'profile':
+        'https://yt3.ggpht.com/ytc/AMLnZu9ouu2eBFlCyh6lORj8YUCdNENJRst9IoU5sYf1Ig=s68-c-k-c0x00ffffff-no-rj',
+    'chname': 'MrBeast',
+    'views': 29,
+    'date': '1 day ago',
+  },
+  {
+    'thumb':
+        'https://i.ytimg.com/vi/15V0gUXUPko/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLASCSlDZZ_fKQeRI7js9_8eeh2dwQ',
+    'duration': '11:18',
+    'title': 'Free Energy Devices Build and Science',
+    'profile':
+        'https://yt3.ggpht.com/ytc/AMLnZu-xiimWu2-uBzreFg2swh-FuuOU6FDbPQDdYD89hA=s68-c-k-c0x00ffffff-no-rj',
+    'chname': 'ElectroBOOM',
+    'views': 5.2,
+    'date': '4 years ago',
+  },
+  {
+    'thumb':
+        'https://i.ytimg.com/vi/13PowoqJkEM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAUGjrCLRoxyMQ-txWgAOeukIKASw',
+    'duration': '9:22',
+    'title': 'Nothing CEO Reviews iPhone 14 Pro',
+    'profile':
+        'https://yt3.ggpht.com/ytc/AMLnZu_oAzmUDis_SX-qi0Wmj-HlfYNgVGSQeLiIkUyr=s88-c-k-c0x00ffffff-no-rj',
+    'chname': 'Nothing',
+    'views': 241,
+    'date': '1 week ago',
+  },
+];
